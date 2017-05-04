1f7b4497bb715dd44d323960f408b9f965eb4ba3
YaoZengzeng <yaozengzeng@zju.edu.cn>
2017-05-04 12:43:27 +0000

    long delay

--- linux-driver/misc-module/jit.c
+++ linux-driver/misc-module/jit.c
@@ -2,10 +2,64 @@
 #include <linux/moduleparam.h>
 #include <linux/init.h>
 
+#include <linux/sched.h>
 #include <linux/time.h>
 #include <linux/timer.h>
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
+#include <linux/types.h>
+#include <linux/spinlock.h>
+#include <linux/interrupt.h>
+
+#include <asm/hardirq.h>
+
+// Use these as data pointers, to implement four files in one function
+enum jit_file {
+	JIT_BUSY,
+	JIT_SCHED,
+	JIT_QUEUE,
+	JIT_SCHEDTO
+};
+
+int delay = HZ;	// The default delay, expressed in jiffies
+
+// This function prints one line of data, after sleeping one second
+// It can sleep in different ways, accoring to the data pointer
+int jit_fn(char *buf, char **start, off_t offset,
+		int len, int *eof, void *data) {
+	unsigned long j0, j1;
+	wait_queue_head_t wait;
+
+	init_waitqueue_head(&wait);
+	j0 = jiffies;
+	j1 = j0 + delay;
+
+	switch((long)data) {
+		case JIT_BUSY:
+			while(time_before(jiffies, j1)) {
+				cpu_relax();
+			}
+			break;
+		case JIT_SCHED:
+			while(time_before(jiffies, j1)) {
+				schedule();
+			}
+			break;
+		case JIT_QUEUE:
+			wait_event_interruptible_timeout(wait, 0, delay);
+			break;
+		case JIT_SCHEDTO:
+			set_current_state(TASK_INTERRUPTIBLE);
+			schedule_timeout(delay);
+			break;
+	}
+	j1 = jiffies; // Actual value after we delayed
+
+	len = sprintf(buf, "%9li %9li\n", j0, j1);
+	*start = buf;
+
+	return len;
+}
 
 // This file, on the other hand, returns the current time forever
 int jit_currentime(char *buf, char **start, off_t offset,
@@ -34,7 +88,10 @@ int jit_currentime(char *buf, char **start, off_t offset,
 
 int __init jit_init(void) {
 	create_proc_read_entry("currentime", 0, NULL, jit_currentime, NULL);
-
+	create_proc_read_entry("jitbusy", 0, NULL, jit_fn, (void *)JIT_BUSY);
+	create_proc_read_entry("jitsched", 0, NULL, jit_fn, (void *)JIT_SCHED);
+	create_proc_read_entry("jitqueue", 0, NULL, jit_fn, (void *)JIT_QUEUE);
+	create_proc_read_entry("jitschedto", 0, NULL, jit_fn, (void *)JIT_SCHEDTO);
 
 	return 0;
 }
